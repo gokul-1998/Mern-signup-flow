e61e5b0876523710be59b6f2c6f43626289bc203
Gokul <gokulakrishnanm1998@gmail.com>
2024-03-07 08:28:30 +0530

    added username, stuck at @apply bug

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import {Link}  from  'react-router-dom' 
+import avatar from  '../assets/profile.png'
+import styles from '../styles/Username.module.css'
+
+
+
 export default function Username() {
   return (
     <div className="container mx-auto">
       <div className="flex  h-screen">
-        <div>
+        <div className={styles.glass}>
           <div className="title flex flex-col items-center">
             <h4 className='text-5xl font-bold'>hello again</h4>
             <span className='py-4 text-xl w-2/3 text-center text-gray-5'>
@@ -14,9 +19,9 @@ export default function Username() {
           </div>
           <form className='py-1'>
             <div className="profile flex justify-center py-4">
-                <img src="" alt="avatar" />
+                <img src={avatar} className={`${styles.profile_img}`} alt="avatar" />
             </div>
-            <div className="textbox">
+            <div className="textbox flex flex-col items-center gap-6">
                 <input type="text" placeholder='Username' />
                 <button type='submit'>Let's Go</button>
             </div>
