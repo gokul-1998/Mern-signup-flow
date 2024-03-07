1ab8d72666f7b08fa8b32131cf55a5cdea22140b
Gokul <gokulakrishnanm1998@gmail.com>
2024-03-07 07:21:02 +0530

    Added code, but css is not correct

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -2,8 +2,28 @@ import React from 'react'
 
 export default function Username() {
   return (
-    <div>
-      Username
+    <div className="container mx-auto">
+      <div className="flex justify-center items-center h-screen">
+        <div>
+          <div className="title flex flex-col items-center">
+            <h4 className='text-5xl font-bold'>hello again</h4>
+            <span className='py-4 text-xl w-2/3 text-center text-gray-5'>
+              Explare more by connecting with us
+            </span>
+
+          </div>
+          <form className='py-1'>
+            <div className="profile flex justify-center py-4">
+                <img src="" alt="avatar" />
+            </div>
+            <div className="textbox">
+                <input type="text" placeholder='Username' />
+                <button type='submit'>Let's Go</button>
+            </div>
+          </form>
+        </div>
+      </div>
+
     </div>
   )
 }
