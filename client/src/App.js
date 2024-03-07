ded6df4e2041750bbde21a2d4be7682549f6abd5
Gokul <gokulakrishnanm1998@gmail.com>
2024-03-07 07:00:18 +0530

    Added basic  components

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,45 @@ import React from 'react'
 import { createBrowserRouter,RouterProvider} from 'react-router-dom'
 
 
+// import all components
+
+import  Username from './components/Username'
+import  Register from './components/Register'
+import Password  from './components/Password'
+import Profile from './components/Profile'
+import Recovery from './components/Recovery'
+import Reset from './components/Reset'
+import PageNotFound from './components/PageNotFound'
+
 // root routes
 const router=createBrowserRouter([
     {
         path : '/',
-        element:<div>Root Route</div>
+        element:<Username/>
     },
     {
         path : '/register',
-        element:<div>Register Route</div>
+        element:<Register></Register>
+    },
+    {
+        path : '/password',
+        element:<Password></Password>
+    },
+    {
+        path : '/profile',
+        element:<Profile></Profile>
+    },
+    {
+        path : '/recovery',
+        element:<Recovery></Recovery>
+    },
+    {
+        path : '/reset',
+        element:<Reset></Reset>
+    },
+    {
+        path : '*',
+        element:<PageNotFound></PageNotFound>
     }
 ])
 
