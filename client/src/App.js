39242d48091d7543db047f184fd9de4c67594ba8
Gokul <gokulakrishnanm1998@gmail.com>
2024-03-07 06:51:58 +0530

    Added init routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { createBrowserRouter,RouterProvider} from 'react-router-dom'
+
+
+// root routes
+const router=createBrowserRouter([
+    {
+        path : '/',
+        element:<div>Root Route</div>
+    },
+    {
+        path : '/register',
+        element:<div>Register Route</div>
+    }
+])
 
 export default function App() {
   return (
-    <div>
-      React Login App
-    </div>
+    <main>
+     <RouterProvider router={router}></RouterProvider>
+   
+
+    </main>
   )
 }
